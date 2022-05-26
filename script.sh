2cbcbc967380b71448b9ae1e7f9aa4ec8e987c87
Yuanhao Ji <jiyuanhao@apache.org>
2022-05-26 19:47:26 +0800

    feat: add support for HTTP code check (#6)
    issue: #4

--- script.sh
+++ script.sh
@@ -23,5 +23,10 @@ request_body="title=${INPUT_TITLE}&body=${INPUT_BODY}&sound=${INPUT_SOUND}&isArc
 echo -e "${cyan}Request url${none}: ${request_url}"
 echo -e "${cyan}Request body${none}:\n${request_body//&/\\n}"
 
-res=$(curl -s -X POST -d "${request_body}" "${request_url}")
-echo -e "${green}Result${none}: ${res}"
+http_code=$(curl -s -o /dev/null -w "%{http_code}" -X POST -d "${request_body}" "${request_url}")
+if [[ $http_code == 200 ]]; then
+  echo -e "${green}Notification sent successfully${none}"
+else
+  echo -e "${red}Request error! The HTTP code is ${http_code}${none}" >&2
+  exit 1
+fi
